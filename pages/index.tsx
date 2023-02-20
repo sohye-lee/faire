c5e62f9a1fd67c96b05af432ebe5f461e515fcb7
sohye-lee <sohyelee@gmail.com>
2023-02-19 23:54:43 -0500

    product list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import * as FeatherIcon from 'react-feather';
+import { RiHeart2Line, RiHeart2Fill, RiAddLine } from 'react-icons/ri';
 
 const Home: NextPage = () => {
   // return (
@@ -104,20 +105,43 @@ const Home: NextPage = () => {
   //     <div className="bg-white p-10 rounded-sm drop-shadow-sm"></div>
   //   </div>
   // );
+
   return (
     <div className="grid grid-cols-5 gap-2  items-stretch w-full p-4">
       {[1, 2, 3, 4, 5, 6, 7].map((_, i) => (
         <div key={i} className="border border-gray-200 h-full">
           <div
-            className="w-full h-0 bg-gray-200"
+            className="w-full h-0 bg-gray-100 relative"
             style={{ paddingTop: 'calc(100% - 2px)' }}
-          ></div>
-          <div className="p-3">
-            <p className="font-serif font-medium text-lg capitalize">title</p>
+          >
+            <RiHeart2Line
+              className="absolute top-2 right-2"
+              width="24"
+              height="24"
+            />
+          </div>
+          <div className="p-3 h-32 flex flex-col justify-between">
+            <div>
+              <p className="font-serif font-bold text-md capitalize">title</p>
+              <p className="font-sans text-sm font-light text-gray-600">
+                Description
+              </p>
+              <p className="font-sans text-sm">$99.99</p>
+            </div>
+            <div className="flex items-center space-x-1">
+              <div className="w-3 h-3 rounded-md bg-blue-400"></div>
+              <div className="w-3 h-3 rounded-md bg-purple-400"></div>
+              <div className="w-3 h-3 rounded-md bg-yellow-400"></div>
+              <div className="w-3 h-3 rounded-md bg-red-400"></div>
+              <div className="w-3 h-3 rounded-md bg-black"></div>
+              <div className="w-3 h-3 rounded-md bg-white border"></div>
+            </div>
           </div>
         </div>
       ))}
-      <button></button>
+      <button className="fixed bottom-10 right-10 bg-black w-10 h-10 flex items-center justify-center rounded-full">
+        <RiAddLine width={60} height={60} color="white" />
+      </button>
     </div>
   );
 };
