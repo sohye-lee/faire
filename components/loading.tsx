5a959b702f5bd8262e9659f786ef08c5b8b14741
sohye-lee <sohyelee@gmail.com>
2023-05-11 19:06:15 -0400

    slider fix

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="w-full h-full max-h-screen  flex items-center justify-center">
+    <div className="w-full h-96 flex items-center justify-center">
       <div className="w-16 h-16 rounded-full  border-2 border-gray-200 border-r-gray-400  animate-spin"></div>
     </div>
   );
